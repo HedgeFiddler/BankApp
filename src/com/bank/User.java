f2cb433f971bb505fa37f85c23cccb550442e25b
Hector <shaba@gmail.com>
2018-08-08 21:29:17 +0300

    'sdf'

--- src/com/bank/User.java
+++ src/com/bank/User.java
@@ -1,139 +1,154 @@
-package com.bank;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
-
-public class User {
-    private String id;
-    private String password;
-    private String fullName;
-    private String address;
-    private List<Account> accounts;
-    private String phoneNumber;
-    private String email;
-    private String secretQuestion;
-    private String answerForSecretQuestion;
-
-    public User(String password,
-                String fullName,
-                String address,
-                String phoneNumber,
-                String email,
-                String secretQuestion,
-                String answerForSecretQuestion) {
-
-        this.id = UUID.randomUUID().toString();
-        this.password = password;
-        this.fullName = fullName;
-        this.address = address;
-        this.phoneNumber = phoneNumber;
-        this.email = email;
-        this.secretQuestion = secretQuestion;
-        this.answerForSecretQuestion = answerForSecretQuestion;
-        this.accounts = new ArrayList<>();
-    }
-
-    public void addNewAccount(String currency){
-        Account newAccount = new Account(currency);
-        accounts.add(newAccount);
-
-    }
-
-    public double getAccountsBalance(){
-        double balanceSum = 0;
-        for (Account account : accounts) {
-            balanceSum += account.getBalance();
-
-        }
-
-        return balanceSum;
-    }
-
-    public void addMoney(long accountId, double amountOfMoney){
-        for (Account account : accounts){
-            if(account.getAccountNumber() == accountId){
-                account.addMoney(amountOfMoney);
-            }
-        }
-    }
-
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getFullName() {
-        return fullName;
-    }
-
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getSecretQuestion() {
-        return secretQuestion;
-    }
-
-    public void setSecretQuestion(String secretQuestion) {
-        this.secretQuestion = secretQuestion;
-    }
-
-    public String getAnswerForSecretQuestion() {
-        return answerForSecretQuestion;
-    }
-
-    public void setAnswerForSecretQuestion(String answerForSecretQuestion) {
-        this.answerForSecretQuestion = answerForSecretQuestion;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id='" + id + '\'' +
-                ", password='" + password + '\'' +
-                ", fullName='" + fullName + '\'' +
-                ", address='" + address + '\'' +
-                ", phoneNumber='" + phoneNumber + '\'' +
-                ", email='" + email + '\'' +
-                ", secretQuestion='" + secretQuestion + '\'' +
-                ", answerForSecretQuestion='" + answerForSecretQuestion + '\'' +
-                '}';
-    }
-}
+//package com.bank;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//import java.util.UUID;
+//
+//public class User {
+//
+//    private String id;
+//    private String password;
+//    private String fullName;
+//    private String address;
+//    private List<Account> accounts;
+//    private String phoneNumber;
+//    private String email;
+//    private String secretQuestion;
+//    private String answerForSecretQuestion;
+//
+//    public User(String password,
+//                String fullName,
+//                String address,
+//                String phoneNumber,
+//                String email,
+//                String secretQuestion,
+//                String answerForSecretQuestion) {
+//
+//        this.id = UUID.randomUUID().toString();
+//        this.password = password;
+//        this.fullName = fullName;
+//        this.address = address;
+//        this.phoneNumber = phoneNumber;
+//        this.email = email;
+//        this.secretQuestion = secretQuestion;
+//        this.answerForSecretQuestion = answerForSecretQuestion;
+//        this.accounts = new ArrayList<>();
+//    }
+//
+//    public void addNewAccount(String currency){
+//        Account newAccount = new Account(currency);
+//        accounts.add(newAccount);
+//
+//    }
+//
+//    public double getAccountsBalance(){
+//        double balanceSum = 0;
+//        for (Account account : accounts) {
+//            balanceSum += account.getBalance();
+//
+//        }
+//
+//        return balanceSum;
+//    }
+//
+//    public void addMoney(long accountId, double amountOfMoney){
+//        if(accounts.size() < 1){
+//            this.addNewAccount("EUR");
+//        }
+//        for (Account account : accounts){
+//            if(account.getAccountNumber() == accountId){
+//                account.addMoney(amountOfMoney);
+//                return;
+//            }
+//        }
+//    }
+//
+//    public void withdrawMoney(long accountId, double amountOfMoney) {
+//        for(Account account : accounts){
+//            if(account.getAccountNumber() == accountId){
+//                account.withdrawMoney(amountOfMoney);
+//                return;
+//            }
+//        }
+//    }
+//
+//
+//    public String getId() {
+//        return id;
+//    }
+//
+//    public void setId(String id) {
+//        this.id = id;
+//    }
+//
+//    public String getPassword() {
+//        return password;
+//    }
+//
+//    public void setPassword(String password) {
+//        this.password = password;
+//    }
+//
+//    public String getFullName() {
+//        return fullName;
+//    }
+//
+//    public void setFullName(String fullName) {
+//        this.fullName = fullName;
+//    }
+//
+//    public String getAddress() {
+//        return address;
+//    }
+//
+//    public void setAddress(String address) {
+//        this.address = address;
+//    }
+//
+//    public String getPhoneNumber() {
+//        return phoneNumber;
+//    }
+//
+//    public void setPhoneNumber(String phoneNumber) {
+//        this.phoneNumber = phoneNumber;
+//    }
+//
+//    public String getEmail() {
+//        return email;
+//    }
+//
+//    public void setEmail(String email) {
+//        this.email = email;
+//    }
+//
+//    public String getSecretQuestion() {
+//        return secretQuestion;
+//    }
+//
+//    public void setSecretQuestion(String secretQuestion) {
+//        this.secretQuestion = secretQuestion;
+//    }
+//
+//    public String getAnswerForSecretQuestion() {
+//        return answerForSecretQuestion;
+//    }
+//
+//    public void setAnswerForSecretQuestion(String answerForSecretQuestion) {
+//        this.answerForSecretQuestion = answerForSecretQuestion;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "User{" +
+//                "id='" + id + '\'' +
+//                ", password='" + password + '\'' +
+//                ", fullName='" + fullName + '\'' +
+//                ", address='" + address + '\'' +
+//                ", accounts=" + accounts +
+//                ", phoneNumber='" + phoneNumber + '\'' +
+//                ", email='" + email + '\'' +
+//                ", secretQuestion='" + secretQuestion + '\'' +
+//                ", answerForSecretQuestion='" + answerForSecretQuestion + '\'' +
+//                '}';
+//    }
+//}
